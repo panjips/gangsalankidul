783056a043c47acd17b3b8d9ef1b2fed16454290
Panji <panjipusaka71@gmail.com>
2024-07-18 19:02:14 +0700

    style(favicon): change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import "@/styles/globals.css";
 import { Providers } from "./providers";
 import { CookiesProvider } from "next-client-cookies/server";
+import Head from "next/head";
 
 const pjs = Plus_Jakarta_Sans({
   weights: [200, 300, 400, 500, 600, 700, 800],
@@ -17,6 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={pjs.className}>
         <Providers>
           <CookiesProvider>{children}</CookiesProvider>
